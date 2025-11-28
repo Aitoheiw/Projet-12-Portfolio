c70c77b1ce60990fdabd16e87a35e43d32d5bb1c
Aitoheiw <124202509+Aitoheiw@users.noreply.github.com>
2025-11-28 18:11:43 +0100

    remove email

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,10 +15,6 @@ export default function About() {
           date with the latest industry trends.
         </p>
         <p>I started my journey in web development a few months ago.</p>
-        <p>
-          If you want to contact me, you can send me an email at:
-          lucascharletttttt@gmail.com
-        </p>
       </div>
     </section>
   );
