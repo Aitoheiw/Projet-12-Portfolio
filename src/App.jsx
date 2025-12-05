1344a1809d91f5d8471d516ca94be045e0679b3e
Aitoheiw <124202509+Aitoheiw@users.noreply.github.com>
2025-12-05 16:47:53 +0100

    test fix dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <div className="bg-white dark:bg-zinc-800 dark:text-white">
       <Home />
-    </>
+    </div>
   );
 }
 
