5bb9b4ea7263f1a0ab5cfd85ed95be842f6a2fc3
Aitoheiw <124202509+Aitoheiw@users.noreply.github.com>
2025-12-11 14:51:43 +0100

    feat : ajout liens linkedin

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,13 +4,16 @@ import { useLanguage } from "../hooks/useLanguage";
 export default function About() {
   const { t } = useLanguage();
   return (
-    <section className="bg-[#f5f5f7] dark:bg-neutral-700/50 pb-10 select-none mx-4 md:mx-6">
-      <h2 className="text-3xl pb-10 pt-5 font-bold">{t.about.title}</h2>
+    <section className="bg-[#f5f5f7] dark:bg-neutral-700/50 pb-10 mx-4 md:mx-6">
+      <h2 className="text-3xl pb-10 pt-5 font-bold select-none">
+        {t.about.title}
+      </h2>
 
       <div className="flex flex-col pb-10 text-xl md:text-2xl gap-5 font-bold pl-4 pr-4">
-        <p>{t.about.text}</p>
-        <p>{t.about.description}</p>
-        <p>{t.about.ambition}</p>
+        <p className="select-none">{t.about.text}</p>
+        <p className="select-none">{t.about.description}</p>
+        <p className="select-none">{t.about.ambition}</p>
+        <p>Linkedin : www.linkedin.com/in/lucas-charlet-0a0183231</p>
       </div>
     </section>
   );
