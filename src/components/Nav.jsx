67ea91aad60bec0381e34fc18dfa0a94020765be
Aitoheiw <124202509+Aitoheiw@users.noreply.github.com>
2025-12-12 22:21:09 +0100

    fix : re order nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,8 +17,8 @@ export default function Nav() {
       {/* 🔹 Desktop menu */}
       <ul className="hidden md:flex justify-end gap-12 text-xl font-bold dark:text-white md:mr-5">
         <li>
-          <a href="#projects" className="nav-link">
-            {t.nav.projects}
+          <a href="#about" className="nav-link">
+            {t.about.title}
           </a>
         </li>
         <li>
@@ -27,10 +27,11 @@ export default function Nav() {
           </a>
         </li>
         <li>
-          <a href="#about" className="nav-link">
-            {t.about.title}
+          <a href="#projects" className="nav-link">
+            {t.nav.projects}
           </a>
         </li>
+
         <li>
           <Link to="/contact" onClick={handleNavClick} className="nav-link">
             {t.nav.contact}
@@ -51,8 +52,8 @@ export default function Nav() {
       {isOpen && (
         <ul className="fixed top-12 right-0 mt-4 flex flex-col gap-6 bg-white dark:bg-neutral-900 shadow-xl p-6 rounded-lg text-lg font-bold z-50">
           <li>
-            <a href="#projects" onClick={() => setIsOpen(false)}>
-              {t.nav.projects}
+            <a href="#about" onClick={() => setIsOpen(false)}>
+              {t.about.title}
             </a>
           </li>
           <li>
@@ -61,10 +62,11 @@ export default function Nav() {
             </a>
           </li>
           <li>
-            <a href="#about" onClick={() => setIsOpen(false)}>
-              {t.about.title}
+            <a href="#projects" onClick={() => setIsOpen(false)}>
+              {t.nav.projects}
             </a>
           </li>
+
           <li>
             <Link to="/contact" onClick={handleNavClick}>
               {t.nav.contact}
